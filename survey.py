db34077f952379b4c85642f7cbdd4aa510c94a55
andrealo2432 <andrealo22432@gmail.com>
2026-04-14 10:25:52 -0500

    loading in questions

---
+++ survey.py
@@ -0,0 +1,18 @@
+def main():
+    return True
+
+def get_question(question_file):
+    question_list = []
+    with open(question_file, "r") as fh:
+        for question in fh:
+            question = question.strip().split(",")
+            question_text = question[0]
+            letter = question[1]
+            question_list.append((question_text, letter))
+    return question_list
+
+def ask_question(question):
+    possible_answers = ()
+
+if __name__ == "__main__":
+    main()
