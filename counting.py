6c34f5aca8c3341f674ac2f850e1327a795d2332
noralz2 <noralz2@illinois.edu>
2026-05-17 22:43:36 -0500

    capitalization edits

--- counting.py
+++ counting.py
@@ -10,18 +10,24 @@ def count(file_path):
             if letter in counts:
                 counts[letter] += 1
     return counts
-#I thought about this function based on the survey.py assignment where we had to 
-#count our results and focused on adding a score based on the letter and making sure the baseline was at zero to start with
+
+
+# I thought about this function based on the survey.py assignment where we had to
+# count our results and focused on adding a score based on the letter and making sure the baseline was at zero to start with
+
 
 def specific_personality(counts):
     a, b, c = counts["a"], counts["b"], counts["c"]
     if a > b and a > c:
-        return "You are the A personality. Despite the possibility of being harmed, you always choose to help."
+        return "you are the A personality. Despite the possibility of being harmed, you always choose to help."
     elif b > a and b > c:
-        return "You are the B personality. You are a practical person who chooses to help when it does not hurt you, but you will not risk yourself unnecessarily."
+        return "you are the B personality. You are a practical person who chooses to help when it does not hurt you, but you will not risk yourself unnecessarily."
     else:
-        return "You are the C personality. You are a focused person, reliant on only your skills to reach your goals."
-#This function was for the specific personalities and making sure based on the score of a's, b's, and c's, only the highest score returns the specific personality distinction based on the counts.
+        return "you are the C personality. You are a focused person, reliant on only your skills to reach your goals."
+
+
+# This function was for the specific personalities and making sure based on the score of a's, b's, and c's, only the highest score returns the specific personality distinction based on the counts.
+
 
 def ties(counts):
     a, b, c = counts["a"], counts["b"], counts["c"]
@@ -36,8 +42,11 @@ def ties(counts):
         results_for_tie = [tie[i] for i in winners]
         return f"Based on the choices you have made in your path, we believe you are a mix of two personalities. We believe you are both {results_for_tie[0]} and {results_for_tie[1]}! We hope these test results let you understand more about who you are and the decisions you tend to make. Thanks for playing our game!"
     return None
-# This function I had to create because in case both letters had the same score. We decided in this special case to have a mix of both personalities rather than just pick one so I created a ties function. 
-# This ties function used the dictionary of counts with the classifications and returns a mix of personalities if there is a tie of the same highest score. 
+
+
+# This function I had to create because in case both letters had the same score. We decided in this special case to have a mix of both personalities rather than just pick one so I created a ties function.
+# This ties function used the dictionary of counts with the classifications and returns a mix of personalities if there is a tie of the same highest score.
+
 
 def results():
     data = count(response_file)
@@ -46,9 +55,10 @@ def results():
         return checking_if_tie
     else:
         personality = specific_personality(data)
-        return f"Based on the choices you have made you align most similar to: {personality} We hope these test results let you understand more about who you are and the decisions you tend to make. Thanks for playing our game!"
+        return f"Based on the choices you have made, {personality}\nWe hope these test results let you understand more about who you are and the decisions you tend to make. Thanks for playing our game!"
 
-# This function was just to show results based on the responses from the response file. 
+
+# This function was just to show results based on the responses from the response file.
 
 if __name__ == "__main__":
     print(results())
